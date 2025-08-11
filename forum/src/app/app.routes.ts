2216a3c943b4fdd48235bad9c6c5ade808a5b38c
Venera <venii13@icloud.com>
2025-08-11 19:57:45 +0300

    add components and routes

--- forum/src/app/app.routes.ts
+++ forum/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component'
+import { HomeComponent } from './home/home.component';
+import {ForumPageComponent } from './forum-page/forum-page.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'forum', component: ForumPageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+];
+
---
+++ forum/src/app/forum-page/forum-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forum-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './forum-page.component.html',
+  styleUrl: './forum-page.component.css'
+})
+export class ForumPageComponent {
+
+}
